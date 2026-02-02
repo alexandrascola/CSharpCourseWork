4783487b3829dbd1bbf333583e7dad1d00a837f4
alexandrascola <alexandrakscola@gmail.com>
2026-02-02 16:02:52 -0600

    Creation of Gradebook Test CS File

---
+++ GradebookSolution/Gradebook.Tests/GradebookTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gradebook.Tests
+{
+    public class GradebookTests
+    {
+        //Succesfully add valid grade
+        [Fact]
+        public void AddGrade_AddsValidGrade()
+        { 
+
+        }
+        //Reject invalid grade
+
+
+        //Get correct average
+
+
+        //Validate highest and lowest grades
+    }
+}
