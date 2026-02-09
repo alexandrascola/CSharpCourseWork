7dc378fe5b71184318276e1bee991124bbfca2cb
alexandrascola <alexandrakscola@gmail.com>
2026-02-09 14:33:36 -0600

    Accessors Complete

--- VehicleFleetManagerApp/VehicleFleetManagerApp/Vehicle.cs
+++ VehicleFleetManagerApp/VehicleFleetManagerApp/Vehicle.cs
@@ -14,12 +14,47 @@ namespace Vehicle_Fleet_Manager
         private double _lastServiceMileage;
 
         //Public properties
-        
-
+        public string Make
+        {
+            get => _make; //accessor method to get the value of _make
+            set => _make = string.IsNullOrWhiteSpace(value)
+                ? throw new ArgumentException("Make cannot be empty.")
+                : value.Trim();
+        }
+        public string Model
+        {
+            get => _model;
+            set => _model = string.IsNullOrWhiteSpace(value)
+                ? throw new ArgumentException("Model cannot be empty.")
+                : value.Trim();
+        }
+        public int Year
+        {
+            get => _year;
+            set => _year = (value <1884 || value > DateTime.Now.Year + 1)
+                ? throw new ArgumentOutOfRangeException(nameof(value), "Year is out of range.")
+                : value;
+        }
+        public double Mileage
+        {
+            get => _mileage;
+            set => _mileage = (value >= 0)
+                ? value
+                : throw new ArgumentOutOfRangeException(nameof(value), "Mileage cannot be negative.");
+        }
+        public double LastServiceMileage
+            {
+            get => _lastServiceMileage;
+            set => _lastServiceMileage = (value >= 0)
+                ? value
+                : throw new ArgumentOutOfRangeException(nameof(value), "Last service mileage must be between 0 and current mileage.");
+        }
 
         //CONSTRUCTORS:
-        //default constructor
-        public Vehicle() { }
+        //Default constructor
+        public Vehicle() { 
+
+        }
 
 
 
