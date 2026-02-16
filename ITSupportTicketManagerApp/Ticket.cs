1ac5318ef2004f06e2f29a81310fa13b5f79f60c
alexandrascola <alexandrakscola@gmail.com>
2026-02-16 14:05:06 -0600

    Ticket.cs Created

---
+++ ITSupportTicketManagerApp/Ticket.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ITSupportTicketManagerApp
+{
+    internal class Ticket
+    {
+        //Class Variables
+        private string _id = "";
+        private string _description = "";
+        private string _priority = "Low";
+        private string _status = "Open";
+
+        //Make 2 Arrays for Allowed Values for Priority and Status
+        public static readonly string[] AllowedPriorities = { "Low", "Medium", "High" };
+        public static readonly string[] AllowedStatuses = { "Open", "In Progress", "Closed" };
+
+
+    }
+}
